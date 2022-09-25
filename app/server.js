638c9f4b4064ca8ad7ac1ced1004af9d0e523572
lidia <li@pu.com>
2022-09-25 21:45:37 +0200

    API modified

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const service = require('./service');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/100jokes', async (req, res) => {
+app.get('/', async (req, res) => {
     let data = await service();
     res.json(data);
 });
